d9243057784cb8e4ffd0ca18302cd6d05b446692
Nitesh Yadav <nitesh.yadav@olacabs.com>
2014-01-23 10:03:48 +0530

    Adding Constants Class

---
+++ src/com/example/mapapp/Constants.java
@@ -0,0 +1,10 @@
+package com.example.mapapp;
+
+public class Constants {
+
+	public static final int TEXT_SIZE_XHDPI = 24;
+	public static final int TEXT_SIZE_HDPI = 20;
+	public static final int TEXT_SIZE_MDPI = 18;
+	public static final int TEXT_SIZE_LDPI = 13;
+
+}
